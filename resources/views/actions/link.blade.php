cec3bd3ac790303adb39ca38ea1a8e41c0a934ae
francoism90 <francoism90@users.noreply.github.com>
2024-04-22 19:25:10 +0200

    wip

--- resources/views/actions/link.blade.php
+++ resources/views/actions/link.blade.php
@@ -3,10 +3,12 @@
     {{ $attributes
         ->cssClass([
             'layer' => 'inline-flex shrink-0 cursor-pointer items-center hover:text-primary-400',
+            'inactive' => 'text-secondary',
             'active' => 'text-primary-400 hover:text-primary-300',
         ])
         ->classMerge([
             'layer',
+            'inactive' => ! $active(),
             'active' => $active(),
         ])
         ->merge([
