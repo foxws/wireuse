0a17a670f6b48348cc172cd3f20f23b86a3c3160
francoism90 <francoism90@users.noreply.github.com>
2024-04-27 17:46:30 +0200

    wip

--- resources/views/actions/icon.blade.php
+++ resources/views/actions/icon.blade.php
@@ -3,16 +3,16 @@
 ])
 
 <div class="inline-flex">
-    @if ($action->hasIcon() && $action->hasActiveState())
+    @if ($action->hasIcon() && $action->hasState())
         <div x-data x-cloak>
             <x-icon
-                x-show="! {{ $action->getActive() }}"
+                x-show="! {{ $action->getState() }}"
                 :name="$action->getIcon()"
                 {{ $attributes->class('size-5') }}
             />
 
             <x-icon
-                x-show="{{ $action->getActive() }}"
+                x-show="{{ $action->getState() }}"
                 :name="$action->getIconActive()"
                 {{ $attributes->class('size-5') }}
             />
--- src/Actions/Concerns/HasState.php
+++ src/Actions/Concerns/HasState.php
@@ -4,11 +4,28 @@ namespace Foxws\WireUse\Actions\Concerns;
 
 trait HasState
 {
+    public function state(string|null $state = null): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
     public function active(bool|string|null $active = null): static
     {
         $this->active = $active;
 
         return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->value('state');
+    }
+
+    public function hasState(): bool
+    {
+        return $this->offsetExists('state');
     }
 
     public function getActive(): mixed
@@ -19,12 +36,5 @@ trait HasState
     public function hasActive(): mixed
     {
         return $this->offsetExists('active');
-    }
-
-    public function hasActiveState(): bool
-    {
-        $active = $this->getActive();
-
-        return is_string($active) && str($active)->startsWith('$wire');
     }
 }
