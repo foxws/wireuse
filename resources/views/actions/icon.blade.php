07755c6eff811dd8819040be0230d7eb7300b753
francoism90 <francoism90@users.noreply.github.com>
2024-04-28 08:17:22 +0200

    wip

--- resources/views/actions/icon.blade.php
+++ resources/views/actions/icon.blade.php
@@ -2,25 +2,23 @@
     'action',
 ])
 
-<div class="inline-flex">
-    @if ($action->hasIcon() && $action->hasState())
-        <div x-data x-cloak>
-            <x-icon
-                x-show="! {{ $action->getState() }}"
-                :name="$action->getIcon()"
-                {{ $attributes }}
-            />
-
-            <x-icon
-                x-show="{{ $action->getState() }}"
-                :name="$action->getIconActive()"
-                {{ $attributes }}
-            />
-        </div>
-    @elseif ($action->hasIcon())
+@if ($action->hasIcon() && $action->hasState())
+    <div x-cloak>
         <x-icon
-            :name="$action->isActive() ? $action->getIconActive() : $action->getIcon()"
+            x-show="! {{ $action->getState() }}"
+            :name="$action->getIcon()"
             {{ $attributes }}
         />
-    @endif
-</div>
+
+        <x-icon
+            x-show="{{ $action->getState() }}"
+            :name="$action->getIconActive()"
+            {{ $attributes }}
+        />
+    </div>
+@elseif ($action->hasIcon())
+    <x-icon
+        :name="$action->isActive() ? $action->getIconActive() : $action->getIcon()"
+        {{ $attributes }}
+    />
+@endif
--- resources/views/actions/link.blade.php
+++ resources/views/actions/link.blade.php
@@ -1,15 +1,9 @@
 <a
-    @if ($action->hasWireModel())
-        wire:click="$set('{{ $action->getWireModel() }}', '{{ $action->getName() }}')"
-    @endif
-
     {{ $attributes
         ->cssClass([
-            'layer' => 'inline-flex shrink-0 cursor-pointer items-center',
+            'layer' => 'inline-flex shrink-0 cursor-pointer items-center justify-center',
             'active' => 'text-primary-400 hover:text-primary-300',
             'inactive' => 'text-secondary hover:text-primary-400',
-            'label' => 'line-clamp-2',
-            'icon' => 'size-5',
         ])
         ->mergeAttributes($action->getBladeAttributes())
         ->classMerge([
@@ -18,16 +12,12 @@
             'inactive' => ! $action->isActive(),
         ])
         ->merge([
+            'x-data' => $action->hasState(),
             'wire:navigate' => $action->useNavigate(),
             'href' => $action->getUrl(),
             'aria-label' => $action->getLabel(),
         ])
     }}
 >
-    @if ($slot->isEmpty())
-        <x-wireuse::actions-icon class="{{ $attributes->classFor('icon') }}" :$action />
-        <span class="{{ $attributes->classFor('label') }}">{{ $action->getLabel() }}</span>
-    @else
-        {{ $slot }}
-    @endif
+    {{ $slot }}
 </a>
