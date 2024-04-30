780a3df84cd655e73b28cbb50b81000c8bbb1a90
francoism90 <francoism90@users.noreply.github.com>
2024-04-30 19:39:40 +0200

    wip

--- resources/views/navigation/tabs.blade.php
+++ resources/views/navigation/tabs.blade.php
@@ -1,11 +1,10 @@
 @php
-    $current = $this->getCurrentNode();
+    $current = $this->getPropertyValue($wireModel() ?? $tabState())
 @endphp
 
-<div {{ $attributes
+<nav {{ $attributes
     ->cssClass([
-        'layer' => 'flex flex-col w-full gap-y-3',
-        'container' => 'flex items-center px-3 gap-x-5 overflow-x-auto border-b border-secondary-500/50',
+        'layer' => 'flex items-center px-3 gap-x-5 overflow-x-auto border-b border-secondary-500/50',
         'tab' => 'py-3 gap-x-1.5 border-b',
         'tab-icon' => 'size-5 sm:size-6',
         'tab-active' => 'border-primary-400 text-primary-400',
@@ -15,37 +14,27 @@
         'layer',
     ])
     ->merge([
-        'wire:model.live' => $this->getNavigationPath(),
+        'wire:model.live' => $tabState(),
         'x-data' => '{ active: null }',
         'x-modelable' => 'active',
     ])
 }}>
-    <nav class="{{ $attributes->classFor('container') }}">
-        @foreach ($nodes as $action)
-            <x-wireuse::actions-link
+    @foreach ($actions as $action)
+        <x-wireuse::actions-link
+            :$action
+            x-on:click="active = '{{ $action->getName() }}'"
+            active="{{ $current }}"
+            class="{{ $attributes->classFor('tab') }}"
+            class:active="{{ $attributes->classFor('tab-active') }}"
+            class:inactive="{{ $attributes->classFor('tab-inactive') }}"
+        >
+            <x-wireuse::actions-icon
                 :$action
-                x-on:click="active = '{{ $action->getName() }}'"
-                active="{{ $current?->getName() }}"
-                class="{{ $attributes->classFor('tab') }}"
-                class:active="{{ $attributes->classFor('tab-active') }}"
-                class:inactive="{{ $attributes->classFor('tab-inactive') }}"
-            >
-                <x-wireuse::actions-icon
-                    :$action
-                    active="{{ $current?->getName() }}"
-                    class:icon="{{ $attributes->classFor('tab-icon') }}"
-                />
+                active="{{ $current }}"
+                class:icon="{{ $attributes->classFor('tab-icon') }}"
+            />
 
-                <span class="line-clamp text-sm font-medium">{{ $action->getLabel() }}</span>
-            </x-wireuse::actions-link>
-        @endforeach
-    </nav>
-
-    @if ($current?->getComponent())
-        <x-dynamic-component :component="$current->getComponent()" :$action />
-    @endif
-
-    @if ($current?->getLivewireComponent())
-        @livewire($current->getLivewireComponent(), ['action' => $current, 'properties' => $this->all()], key($current->getName()))
-    @endif
-</div>
+            <span class="line-clamp text-sm font-medium">{{ $action->getLabel() }}</span>
+        </x-wireuse::actions-link>
+    @endforeach
+</nav>
--- src/Navigation/Components/Tab.php
+++ src/Navigation/Components/Tab.php
@@ -3,25 +3,21 @@
 namespace Foxws\WireUse\Navigation\Components;
 
 use Closure;
+use Foxws\WireUse\Actions\Support\Action;
 use Foxws\WireUse\States\Support\State;
 use Foxws\WireUse\Views\Support\Component;
 use Illuminate\View\View;
 
-class Tabs extends Component
+class Tab extends Component
 {
     public function __construct(
+        public Action $action,
         public State $state,
-        public array $actions,
     ) {
     }
 
     public function render(): View|Closure|string
     {
-        return view('wireuse::navigation.tabs');
-    }
-
-    public function tabState(): ?string
-    {
-        return implode('.', [$this->state->getPropertyName(), 'tab']);
+        return view('wireuse::navigation.tab');
     }
 }
