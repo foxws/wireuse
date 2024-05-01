613ecba2a21556e6cf2aa3c5ebfad451cb368d7a
francoism90 <francoism90@users.noreply.github.com>
2024-05-01 12:22:05 +0200

    wip

--- resources/views/actions/button.blade.php
+++ resources/views/actions/button.blade.php
@@ -1,10 +1,12 @@
 <button {{ $attributes
     ->cssClass([
         'layer' => 'inline-flex shrink-0 cursor-pointer items-center justify-center',
+        'primary' => 'py-1.5 px-3 bg-primary-500 rounded border border-primary-500',
     ])
     ->mergeAttributes($action->getComponentAttributes())
     ->classMerge([
         'layer',
+        'primary' => $attributes->get('type') === 'submit',
     ])
     ->merge([
         'wire:model' => $action->getWireModel(),
