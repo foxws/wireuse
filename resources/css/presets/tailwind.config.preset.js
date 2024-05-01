1260d53cb57adfc23a3c9023e1ad51f1d3aa518c
francoism90 <francoism90@users.noreply.github.com>
2024-05-01 16:54:58 +0200

    wip

--- resources/css/presets/tailwind.config.preset.js
+++ resources/css/presets/tailwind.config.preset.js
@@ -3,6 +3,6 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./resources/**/*.blade.php', './src/**/*.php', './vendor/foxws/wireuse/**/*.blade.php'],
+  content: ['./resources/**/*.blade.php', './app/View/**/*.php', './src/**/*.php', './vendor/foxws/wireuse/**/*.blade.php'],
   plugins: [forms, typography],
 };
