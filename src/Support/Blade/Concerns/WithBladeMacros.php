cfbc4fd32d5bac49a25cbd54290a66e5cafcec13
francoism90 <francoism90@users.noreply.github.com>
2024-07-12 10:25:49 +0200

    wip

---
+++ src/Support/Blade/Concerns/WithBladeMacros.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Foxws\WireUse\Support\Blade\Concerns;
+
+use Foxws\WireUse\Support\Blade\Bladeable;
+use Illuminate\View\ComponentAttributeBag;
+
+trait WithBladeMacros
+{
+    protected function registerBladeMacros(): static
+    {
+        ComponentAttributeBag::macro('cssClass', function (array $values = []): mixed {
+            foreach ($values as $key => $value) {
+                $key = Bladeable::classKeys($key)->first();
+
+                /** @var ComponentAttributeBag $this */
+                if (! $this->has($key)) {
+                    $this->offsetSet($key, $value);
+                }
+            }
+
+            return $this;
+        });
+
+        ComponentAttributeBag::macro('classMerge', function (?array $values = null): mixed {
+            /** @var ComponentAttributeBag $this */
+            $classes = Bladeable::classMerged($this, $values)
+                ->merge($this->get('class'))
+                ->join(' ');
+
+            $this->offsetSet('class', $classes);
+
+            return $this
+                ->withoutClass();
+        });
+
+        ComponentAttributeBag::macro('withoutClass', function (): mixed {
+            /** @var ComponentAttributeBag $this */
+            return $this
+                ->whereDoesntStartWith('class:');
+        });
+
+        ComponentAttributeBag::macro('withoutWireModel', function (): mixed {
+            /** @var ComponentAttributeBag $this */
+            return $this
+                ->whereDoesntStartWith('wire:model');
+        });
+
+        ComponentAttributeBag::macro('mergeAttributes', function (array $values = []): mixed {
+            foreach ($values as $key => $value) {
+                /** @var ComponentAttributeBag $this */
+                $this->offsetSet($key, $value);
+            }
+
+            return $this;
+        });
+
+        ComponentAttributeBag::macro('classFor', function (string $key, string $default = ''): mixed {
+            /** @var ComponentAttributeBag $this */
+            $class = Bladeable::classKeys($key)->first();
+
+            return $this->get($class, $default);
+        });
+
+        ComponentAttributeBag::macro('wireModel', function (): mixed {
+            /** @var ComponentAttributeBag $this */
+            return $this->whereStartsWith('wire:model')->first();
+        });
+
+        ComponentAttributeBag::macro('wireKey', function (): mixed {
+            /** @var ComponentAttributeBag $this */
+            return $this->wireModel() ?: $this->first('id') ?: $this->first('name');
+        });
+
+        return $this;
+    }
+}
