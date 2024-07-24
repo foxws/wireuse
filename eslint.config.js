ff8469d608bb0fd71198beadcb9f314da18a9a80
francoism90 <francoism90@users.noreply.github.com>
2024-07-24 20:52:54 +0200

    Fix styling

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules/", "vendor/", "storage/", "public/"],
+  },
+];
