27a37b71155b304e9d6b90925fcf82e9af25548e
François M. <francoism90@users.noreply.github.com>
2024-07-29 14:00:28 +0200

    Update WireUse.php

--- src/Facades/WireUse.php
+++ src/Facades/WireUse.php
@@ -6,12 +6,6 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Foxws\WireUse\WireUse
- *
- * @method static void registerComponents(string $name, string $namespace, string $prefix, ?Closure $callback = null)
- * @method static void registerLivewireComponents(string $name, string $namespace, string $prefix, ?Closure $callback = null)
- * @method static Stringable componentName(DiscoveredClass $class, string $namespace, string $prefix)
- * @method static Stringable componentNamespace(DiscoveredClass $class, string $namespace)
- * @method static string componentPrefix(string $prefix)
  */
 class WireUse extends Facade
 {
