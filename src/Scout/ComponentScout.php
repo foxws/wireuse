a6d9b7eaac6a717273c9182cbfd201b0399599f1
francoism90 <francoism90@users.noreply.github.com>
2024-07-29 12:56:34 +0200

    wip

--- src/Scout/ComponentScout.php
+++ src/Scout/ComponentScout.php
@@ -3,65 +3,16 @@
 namespace Foxws\WireUse\Scout;
 
 use Foxws\WireUse\Support\Discover\ComponentStructureScout;
-use Illuminate\Support\Collection;
-use Spatie\StructureDiscoverer\Data\DiscoveredStructure;
+use Illuminate\Support\Facades\Blade;
 
-class ComponentScout
+class ComponentScout extends Scout
 {
-    public function __construct(
-        public ?string $path = null,
-        public ?string $namespace = null,
-        public ?string $prefix = null,
-    ) {}
-
-    public static function create(
-        string $path,
-        string $namespace = 'App\\',
-        ?string $prefix = null,
-    ): self {
-        return new self(
-            path: $path,
-            namespace: $namespace,
-            prefix: $prefix,
-        );
-    }
-
-    public function get(): Collection
+    public function register(): void
     {
-        $scout = $this->getComponentStructures();
-
-        return Collection::make($scout->get())
-            ->map(fn (DiscoveredStructure $class) => [
-                'class' => $class->getFcqn(),
-                'name' => $this->componentName($class),
-            ]);
-    }
-
-    protected function componentName(DiscoveredStructure $class): string
-    {
-        return str($class->name)
-            ->kebab()
-            ->prepend(
-                $this->componentPrefix(),
-                $this->componentNamespace($class)
-            );
-    }
-
-    protected function componentPrefix(): string
-    {
-        return str($this->prefix)
-            ->replace('\\', '.')
-            ->kebab();
-    }
-
-    protected function componentNamespace(DiscoveredStructure $class): string
-    {
-        return str($class->namespace)
-            ->after($this->namespace)
-            ->match('/(.*)\\\\/')
-            ->replace('\\', '.')
-            ->slug('.')
-            ->finish('.');
+        $this->get()->each(fn (array $component) => Blade::component(
+            class: $component['class'],
+            alias: $component['name'],
+        ));
     }
 
     protected function getComponentStructures(): ComponentStructureScout
--- src/Scout/LivewireScout.php
+++ src/Scout/LivewireScout.php
@@ -3,102 +3,19 @@
 namespace Foxws\WireUse\Scout;
 
 use Foxws\WireUse\Support\Discover\LivewireStructureScout;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 use Livewire\Livewire;
-use Spatie\StructureDiscoverer\Data\DiscoveredStructure;
 
-class LivewireScout
+class LivewireScout extends Scout
 {
-    public function __construct(
-        public ?string $path = null,
-        public ?string $namespace = null,
-        public ?string $prefix = null,
-    ) {}
-
-    public static function create(
-        string $path,
-        string $namespace = 'App\\',
-        ?string $prefix = null,
-    ): self {
-        return new self(
-            path: $path,
-            namespace: $namespace,
-            prefix: $prefix,
-        );
-    }
-
     public function register(): void
     {
         $this->get()->each(fn (array $component) => Livewire::component(...$component));
     }
 
-    public function get(): Collection
-    {
-        return Cache::store($this->getCacheStore())->remember(
-            $this->getCacheKey(),
-            $this->getCacheLifetime(),
-            fn () => $this->get()
-        );
-    }
-
-    protected function buildCollection(): Collection
-    {
-        $scout = $this->getLivewireStructures();
-
-        return Collection::make($scout->get())
-            ->map(fn (DiscoveredStructure $class) => [
-                'class' => $class->getFcqn(),
-                'name' => $this->componentName($class),
-            ]);
-    }
-
-    protected function componentName(DiscoveredStructure $class): string
-    {
-        return str($class->name)
-            ->kebab()
-            ->prepend(
-                $this->componentPrefix(),
-                $this->componentNamespace($class)
-            );
-    }
-
-    protected function componentPrefix(): string
-    {
-        return str($this->prefix)
-            ->replace('\\', '.')
-            ->kebab();
-    }
-
-    protected function componentNamespace(DiscoveredStructure $class): string
-    {
-        return str($class->namespace)
-            ->after($this->namespace)
-            ->match('/(.*)\\\\/')
-            ->replace('\\', '.')
-            ->slug('.')
-            ->finish('.');
-    }
-
-    protected function getLivewireStructures(): LivewireStructureScout
+    protected function getComponentStructures(): LivewireStructureScout
     {
         return LivewireStructureScout::create()
             ->path($this->path)
             ->prefix("livewire-structures-{$this->prefix}");
-    }
-
-    protected function getCacheKey(): string
-    {
-        return "wireuse.scout.livewire.{$this->prefix}";
-    }
-
-    protected function getCacheStore(): ?string
-    {
-        return config('wireuse.scout.cache_store');
-    }
-
-    protected function getCacheLifetime(): ?int
-    {
-        return config('wireuse.scout.cache_lifetime');
     }
 }
