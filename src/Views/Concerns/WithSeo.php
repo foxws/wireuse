a5ad375a21c9dd7f9ca101a02762e80e303bcbba
François M. <francoism90@users.noreply.github.com>
2024-08-15 11:09:49 +0200

    Update WithSeo.php

--- src/Views/Concerns/WithSeo.php
+++ src/Views/Concerns/WithSeo.php
@@ -3,14 +3,15 @@
 namespace Foxws\WireUse\Views\Concerns;
 
 use Artesaos\SEOTools\Facades\SEOMeta;
+use Closure;
 
 trait WithSeo
 {
     public function bootWithSeo(): void
     {
-        SEOMeta::setTitle($this->seoValue('getTitle'));
-        SEOMeta::setDescription($this->seoValue('getDescription'));
-        SEOMeta::setRobots($this->seoValue('getRobots'));
+        SEOMeta::setTitle((string) $this->seoValue('getTitle'));
+        SEOMeta::setDescription((string) $this->seoValue('getDescription'));
+        SEOMeta::setRobots((string) $this->seoValue('getRobots'));
     }
 
     protected function seoValue(mixed $value, mixed $default = null): mixed
@@ -20,7 +21,9 @@ trait WithSeo
         }
 
         if (method_exists(static::class, $value)) {
-            return strip_tags($this->$value());
+            $value = $this->$value();
+
+            return is_string($value) ? strip_tags($value) : $value;
         }
 
         return $default;
