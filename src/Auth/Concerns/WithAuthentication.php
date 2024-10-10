4f0aac574d012d655f8b058be377c4205e4f2ba4
François M. <francoism90@users.noreply.github.com>
2024-10-10 17:49:29 +0200

    Update WithAuthentication.php

--- src/Auth/Concerns/WithAuthentication.php
+++ src/Auth/Concerns/WithAuthentication.php
@@ -11,14 +11,14 @@ trait WithAuthentication
         return auth()->check();
     }
 
-    protected function getAuthUser(): ?User
-    {
-        return auth()->user();
-    }
-
     protected function getAuthId(): int|string|null
     {
         return auth()->id();
+    }
+
+    protected function getAuthModel(): ?User
+    {
+        return auth()->user();
     }
 
     protected function getAuthKey(): int|string|null
