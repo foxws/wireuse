f965cfea73d8252649e5f7c33a48b40c641d724e
François M. <francoism90@users.noreply.github.com>
2024-10-10 17:50:30 +0200

    Values may be null

--- src/Exceptions/RateLimitedException.php
+++ src/Exceptions/RateLimitedException.php
@@ -7,8 +7,8 @@ use Exception;
 class RateLimitedException extends Exception
 {
     public function __construct(
-        public string $ip,
-        public int $seconds,
+        public ?string $ip = null,
+        public ?int $seconds = null,
     ) {
         parent::__construct(sprintf(
             'Too many requests from [%s]. Retry in %d seconds.',
