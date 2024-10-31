435e4c2e3a1fd0a5739c0be584ea44a8f673c959
francoism90 <francoism90@users.noreply.github.com>
2024-10-31 17:45:47 +0100

    Add Macroable

--- src/Views/Support/Component.php
+++ src/Views/Support/Component.php
@@ -5,11 +5,13 @@ namespace Foxws\WireUse\Views\Support;
 use Foxws\WireUse\Views\Concerns\WithHash;
 use Foxws\WireUse\Views\Concerns\WithLivewire;
 use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
 use Illuminate\View\Component as BaseComponent;
 
 abstract class Component extends BaseComponent
 {
     use Conditionable;
+    use Macroable;
     use WithHash;
     use WithLivewire;
 }
