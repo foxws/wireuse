7933c5b36d7476d7184eacc7e959a1afe0e8065a
francoism90 <francoism90@users.noreply.github.com>
2024-10-31 17:32:17 +0100

    wip

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,7 +6,6 @@ expect()
     ->extend('toBeSameModel', fn (Model $model) => $this->is($model)->toBeTrue());
 
 beforeEach(function () {
-    // Fake instances
     \Illuminate\Support\Facades\Bus::fake();
     \Illuminate\Support\Facades\Mail::fake();
     \Illuminate\Support\Facades\Notification::fake();
