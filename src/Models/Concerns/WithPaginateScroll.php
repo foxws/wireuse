b0b6225c6b1dc5da3d5534d77e403f26ce8780ef
François M. <francoism90@users.noreply.github.com>
2024-11-01 13:49:39 +0100

    Create WithPaginateScroll.php

---
+++ src/Models/Concerns/WithPaginateScroll.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Foxws\WireUse\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\Paginator;
+use Laravel\Scout\Scout;
+use Livewire\WithPagination;
+
+trait WithPaginateScroll
+{
+    use WithQueryBuilder;
+    use WithPagination;
+    use WithScroll;
+
+    public function bootWithPaginateScroll(): void
+    {
+        throw_if(
+            ! method_exists($this, 'getBuilder') ||
+            ! ($this->getBuilder() instanceof Builder || ! $this->getBuilder() instanceof Scout)
+        );
+    }
+
+    public function updatedPage(): void
+    {
+        $this->fetch();
+    }
+
+    public function isFetchable(): bool
+    {
+        if ($this->items->isEmpty()) {
+            return true;
+        }
+
+        return $this->getBuilder()->hasMorePages();
+    }
+
+    public function clear(): void
+    {
+        $this->reset('fetchCount');
+
+        $this->resetPage();
+
+        $this->models = collect();
+
+        unset($this->items);
+    }
+
+    protected function getMergeCandidates(): Collection
+    {
+        return $this->getBuilder()->getCollection();
+    }
+
+    protected function getBuilder(): Paginator
+    {
+        return $this->getQuery()
+            ->simplePaginate(
+                perPage: $this->getCandidatesLimit(),
+                page: $this->getPage(),
+            );
+    }
+}
